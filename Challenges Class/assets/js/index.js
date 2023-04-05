f9e17a7faf13f36f95d39ae8d7063c1ebd03d7f6
sugiartofahmi <sugiartofahmi@gmail.com>
2023-04-06 04:12:26 +0700

    Fahmi Sugiarto | Completed Section 2

---
+++ Challenges Class/assets/js/index.js
@@ -0,0 +1,14 @@
+// Section 2
+const byIdName = document.getElementById("name");
+const byIdEmail = document.getElementById("email");
+const byIdAge = document.getElementById("age");
+
+const byNameName = document.getElementsByName("name");
+const byNameEmail = document.getElementsByName("email");
+const byNameAge = document.getElementsByName("age");
+
+const byTagForm = document.getElementsByTagName("input");
+
+const byClassForm = document.getElementsByClassName("input-form");
+
+const bySelectorForm = document.querySelectorAll("input");
